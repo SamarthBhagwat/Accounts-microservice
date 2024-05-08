5dae72800e421788b247ec3e25b6f4ca93815f6d
Samarth <samarthrn19@gmail.com>
2024-05-09 00:32:45 +0530

    Initial commit with hello world endpoint

---
+++ src/test/java/com/example/bankapplication/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankapplication.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
